b7275b63f24a04951990c86b57f2f0c6a4f1309c
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-22 00:31:23 -0500

    Added CFBD API request to index action to obtain all recruits data for a given year // updated http requests to render recruits data

--- app/controllers/recruits_controller.rb
+++ app/controllers/recruits_controller.rb
@@ -1,5 +1,28 @@
+require "net/http"
+require "uri"
+
 class RecruitsController < ApplicationController
   def index
+    # Web request
+    # Specifically: all ranked recruits for a given year in order of ranking
+    uri = URI.parse("https://api.collegefootballdata.com/recruiting/players?year=2021&classification=HighSchool")
+    request = Net::HTTP::Get.new(uri)
+    request["Accept"] = "application/json"
+    request["Authorization"] = "Bearer #{Rails.application.credentials.cfbd_api_key}"
+
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    # Web response
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    # Accessing all recruits
+    all_recruits = JSON.parse(response.body)
+
+    render json: all_recruits
   end
 
   def show
