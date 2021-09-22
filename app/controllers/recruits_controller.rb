caa2c6fad79e3149f2707ff01d5f48b2e4d3e580
Kiel Arneson <kiel.arneson@gmail.com>
2021-09-22 02:19:45 -0500

    Created cfbd_api_response method to DRY code for index and show actions

--- app/controllers/recruits_controller.rb
+++ app/controllers/recruits_controller.rb
@@ -2,7 +2,7 @@ require "net/http"
 require "uri"
 
 class RecruitsController < ApplicationController
-  def index
+  def cfbd_api_response
     # Web request
     # Specifically: all ranked recruits for a given year in order of ranking
     uri = URI.parse("https://api.collegefootballdata.com/recruiting/players?year=2021&classification=HighSchool")
@@ -19,34 +19,20 @@ class RecruitsController < ApplicationController
       http.request(request)
     end
 
+    cfbd_api_response = JSON.parse(response.body)
+    return cfbd_api_response
+  end
+
+  def index
     # Accessing all recruits
-    all_recruits = JSON.parse(response.body)
+    all_recruits = cfbd_api_response()
 
     render json: all_recruits
   end
 
   def show
-    # Web request
-    # Specifically: all ranked recruits for a given year in order of ranking
-    uri = URI.parse("https://api.collegefootballdata.com/recruiting/players?year=2021&classification=HighSchool")
-    request = Net::HTTP::Get.new(uri)
-    request["Accept"] = "application/json"
-    request["Authorization"] = "Bearer #{Rails.application.credentials.cfbd_api_key}"
-
-    req_options = {
-      use_ssl: uri.scheme == "https",
-    }
-
-    # Web response
-    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-      http.request(request)
-    end
-
     # Accessing all recruits
-    all_recruits = JSON.parse(response.body)
-
-    # Accessing specific recruit
-    specific_recruit = all_recruits.select { |player| player["name"].downcase == params[:name].downcase }
+    specific_recruit = cfbd_api_response().select { |player| player["name"].downcase == params[:name].downcase }
     render json: specific_recruit
   end
 end
